24b1522decc7aae854b663e3dc413ab5fabf6468
Wesley Hansen <wesrhansen@gmail.com>
2026-06-06 12:10:26 -0400

    get images served properly

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("voting.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
+]
